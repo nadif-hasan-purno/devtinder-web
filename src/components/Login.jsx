7ceb7e7dd85af00ef9feeeadeb179f0a9c42736f
Nadif Hasan Purno <57897673+nadif-hasan-purno@users.noreply.github.com>
2025-05-01 15:45:24 +0600

    build logout features

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,6 +9,7 @@ import { BASE_URL } from "../utils/constants";
 const Login = () => {
   const [emailId, setEmailId] = useState("brad@gmail.com");
   const [password, setPassword] = useState("Purno@1234");
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -25,7 +26,7 @@ const Login = () => {
       dispatch(addUser(res.data));
       navigate("/");
     } catch (err) {
-      console.log(err);
+      setError(err?.response?.data || "Something went wrong");
     }
   };
 
@@ -59,6 +60,7 @@ const Login = () => {
               />
             </fieldset>
           </div>
+          <p className="text-red-500">{error}</p>
           <div className="card-actions justify-center m-2">
             <button className="btn btn-primary" onClick={handleLogin}>
               Login
